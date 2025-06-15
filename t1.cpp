173c334aff6403f2a42ddc7bb44b014eaa317fdb
Mester-Tak <yahyaeiarian88@gmail.com>
2025-06-15 13:02:30 +0330

    Divisibility

---
+++ t1.cpp
@@ -0,0 +1,36 @@
+//بخش پذیری
+#include <iostream>
+using namespace std;
+main(){
+
+
+int a,b;
+float c;
+string d;
+
+line11:
+cout<<"please enter a:";
+cin>>a;
+   if (a>0)
+   {
+    cout<<"please enter b:";
+    cin>>b;
+    goto line25;
+   }
+    else 
+    cout<<"please enter larger then '0'"<<endl<<endl;
+    goto line11;
+    
+    
+line25:    
+c=(a%b); 
+
+if (c==0)
+{
+    cout<<"Bakhsh pazir ast ^_^"<<'\a'<<endl;
+}
+    else
+    cout<<"Bakhsh pazir nist :\\"<<'\a'<<endl;
+     
+    return 0;
+}
